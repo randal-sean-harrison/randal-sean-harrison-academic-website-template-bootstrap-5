04cb2e05d250d936f4e522903e0cb591f5bdb1c0
Randal Sean Harrison <rharriso@nd.edu>
2025-10-22 00:13:33 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+
+   // Load the top page navbar
+   $("#page-navbar").load("navbar.html", function () {
+      console.log("Navbar loaded.");
+   });
+
+   // Write the page footer
+   $("#footer").load("footer.html", function () {
+      console.log("Footer loaded.");
+
+      // Add the current year to copyright
+      const thisYear = new Date().getFullYear();
+      $("#footer-year").text(thisYear);
+   });
+
+   // Load the acknowledge footer
+   $("#acknowledge").load("acknowledge.html", function () {
+      console.log("Acknowledge banner loaded.");
+   });
+
+   // Scroll to top button ---------------------------------------------
+
+   // write the button to the page
+   window.onscroll = function () {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+         document.getElementById("back-to-top").style.display = "block";
+      } else {
+         document.getElementById("back-to-top").style.display = "none";
+      }
+   };
+
+   // Scroll to top
+   $("#back-to-top").on("click", function (event) {
+      if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+         // Figure out element to scroll to
+         var target = $(this.hash);
+         target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+         // Does a scroll target exist?
+         if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $("html, body").animate(
+               {
+                  scrollTop: target.offset().top,
+               },
+               300,
+               function () {
+                  // Callback after animation
+                  // Must change focus!
+                  var $target = $(target);
+                  $target.focus();
+                  if ($target.is(":focus")) {
+                     // Checking if the target was focused
+                     return false;
+                  } else {
+                     $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                     $target.focus(); // Set focus again
+                  }
+               }
+            );
+         }
+      }
+   });
+
+
+});
+// document.ready closes
